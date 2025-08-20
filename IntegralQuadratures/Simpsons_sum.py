6257b60750271e5abf7b400426182566d09803da
Jay Mayfield <jay.mayfield91@gmail.com>
2025-08-20 17:03:14 -0400

    Changed Simpson_sum.py

--- IntegralQuadratures/Simpsons_sum.py
+++ IntegralQuadratures/Simpsons_sum.py
@@ -7,21 +7,38 @@ def func(y):
 	y = np.array(y) # make sure this is a numpy array
 	return np.sqrt(y) - y
 
-def Trap_sum(a,b,x,N):
+def Simp_sum(a,b,N):
+	if N % 2 != 0:
+		print("Simpsons Rule requires N be even!")
+		print("Leaving script...")
+		sys.exit()
+	x = np.linspace(a,b,N+1)
 	dx = (b-a)/N
-	summ = 0.0
-	for i in range(0,N):
-		summ = summ + (func(x[i]) + func(x[i+1]))/2.0
-	summ = dx*summ
+
+	# add the endpoints
+	summ = func(x[0]) + func(x[N])
+
+	# sum of odd interior nodes
+	summ += 4*np.sum(func(x[1:N:2]))
+
+	# sum of even interior nodes
+	summ += 2*np.sum(func(x[2:N-1:2]))
+
+	summ *= dx/3.0
+
+#	summ = 0.0
+#	for i in range(0,N,2):
+#		summ = summ + func(x[i]) + 4*func((x[i]+x[i+1])/2.0) + func(x[i+1])
+#	summ = dx/3.0*summ
 	print(dx)
 	print(summ)
 
 a, b = 1, 23 # endpoints of interval
 N = 50 # number of rectangles
-n = N + 1 # number of nodes in mesh
+#n = N + 1 # number of nodes in mesh
 
 
-x = np.linspace(a,b,n)
+# x = np.linspace(a,b,n)
 
-Trap_sum(a,b,x,N)
+Simp_sum(a,b,N)
 
