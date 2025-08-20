f0ae3151dc35c311f32d97c3da4fe849103d6f1e
Jay Mayfield <jay.mayfield91@gmail.com>
2025-08-20 15:18:39 -0400

    rewrote bisection_method.py to include more functions

--- RootFinding/bisection_method.py
+++ RootFinding/bisection_method.py
@@ -16,15 +16,29 @@ def sign(x):
 	else:
 		return 0
 
+# one iteration of the bisection method
+def bisection(a, b, TOL, n):
+	c = (a+b)/2.0 # new midpoint
+	if (func(c)==0 or ((b-a)/2.0)<TOL): #solution found
+		print("Solution found!")
+		print(f"Root = {c}")
+		print(f"f(c) = {func(c)}")
+		print(f"Number of iterations = {n-1}")
+		sys.exit()
+	
+	if(sign(func(a)) == sign(func(c))):
+		a = c
+	elif(sign(func(b)) == sign(func(c))):
+		b = c
+	else:
+		print("what...")
+		sys.exit()
+	return a,b,c
+
 a, b = -2, 2	# bounds of interval
-N = 1000	# number of nodes in interval
-iterMAX = 1000	# max number of iterations of bisection method
+iterMAX = 5	# max number of iterations of bisection method
 TOL = 0.001	# smallest value the interval is allowed to be
 
-x = np.linspace(a,b,N)	# array of discrete domain
-
-# array of function
-f = func(x)
 
 # checking if a root in the interval is possible
 if (func(a)<=0 and func(b)<=0) or (func(a)>=0 and func(b)>=0):
@@ -33,22 +47,14 @@ if (func(a)<=0 and func(b)<=0) or (func(a)>=0 and func(b)>=0):
 	sys.exit()
 
 # Bisection method
-n = 1 # used to count iterations of bisection method
+n = 0 # used to count iterations of bisection method
 while (n<=iterMAX):
-	c = (a+b)/2.0 #new midpoint
-	if (func(c)==0) or ((b-a)/2.0<TOL):
-		print("Solution found!")
-		print(f"Root = {c}")
-		print(f"Number of iterations = {n}")
-		print(f"f(c) = {func(c)}")
-		sys.exit()
 	n = n+1
-	if (sign(func(a)) == sign(func(c))):
-		a = c
-	elif (sign(func(b)) == sign(func(c))):
-		b = c
-	else:
-		print("what...")
+	[a,b,c] = bisection(a,b,TOL,n)
 
+if (n > iterMAX):
+	print("Maximum iterations obtained.")
+	print(f"Approximate root = {c}")
+	sys.exit()
 
 
