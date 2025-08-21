409777a2d736d79a3c54b34dd6eb79c1e30e2ee7
Jay Mayfield <jay.mayfield91@gmail.com>
2025-08-21 15:36:01 -0400

    changes the NewtonsMethod.py

--- RootFinding/NewtonsMethod.py
+++ RootFinding/NewtonsMethod.py
@@ -3,25 +3,47 @@
 
 import numpy as np
 
+##################
+# Function of which we wish to find the root of
+# Input: x -- any element in the finite interval
+# Output: the function evaluated at input x
+##################
 def func(x):
-	return np.exp(x) - 2.0
+	return np.sin(x) - x**2/5.0
 
+##################
+# Derivative of the function from func(x)
+# Input: x -- any element in the finite interval
+# Output: the derivative of the func(x) at the input x
+##################
 def dfunc(x):
-	return np.exp(x)
+	return np.cos(x) - (2.0/5.0)*x
 
-def NewtonIter(x0):
-	return x0 - func(x0)/dfunc(x0)
+##################
+# Newton's iterative method
+# Input: x0      -- initial guess to the root
+#	 matIter -- the maximum number of allowed Newton Iterations
+#	 TOL     -- tolerance between func(x) and 0
+# Output: x0 -- Approximate root of the function from func(x) in the given interval
+#	  n  -- number of iterations to match TOL	  
+##################
+def Newton(x0, maxIter, TOL):
+	n = 0 # counter
+	x1 = 0.0
+	while (n < maxIter) and abs(func(x0)) > TOL:
+		n += 1
+		x1 = x0 - func(x0)/dfunc(x0)
+		x0 = x1
+	return x0, n
 
 #initial guess
 x0 = 5
+# max iterations
+maxIter = 1e2
+# tolerance of the approximate root
+TOL = 1e-9
 
-maxIter = 50
-
-n = 0 #counter
-while n <= maxIter:
-	n +=1
-	x0 = NewtonIter(x0)
+[x0, iters] = Newton(x0, maxIter, TOL)
 
 print(f"Root = {x0}")
-print(np.log(2))
-
+print(f"Number of iterations: {iters}")
